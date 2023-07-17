9da367f5a1f1227a9f7283b6bfb1e3a19d1ee35f
milos970 <dev980lop@gmail.com>
2023-07-17 10:09:55 +0200

    First commit

---
+++ src/javaapplication1/Command.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Milos
+ */
+public enum Command {
+    ON_BATTERY_DO_NOTHING("powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0"),
+    ON_BATTERY_SLEEP("powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1"),
+    PLUGGED_IN_DO_NOTHING("powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0"),
+    PLUGGED_IN_SLEEP("powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1");
+    private final String text;
+    
+    private Command(String text) {
+        this.text = text;
+    }
+    
+    public String getText() {
+        return this.text;
+    }
+    
+}
