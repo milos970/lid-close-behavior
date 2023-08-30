9d1068d1bb38f6e987c41c621edfd026e9a4e02a
milos970 <dev980lop@gmail.com>
2023-08-30 20:25:09 +0200

    Refactoring

--- src/javaapplication1/Command.java
+++ src/javaapplication1/Command.java
@@ -15,7 +15,7 @@ public enum Command {
     PLUGGED_IN_SLEEP("powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1");
     private final String text;
     
-    private Command(String text) {
+    Command(String text) {
         this.text = text;
     }
     
